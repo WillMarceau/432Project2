877900ebab90cee665890a6f889f70cf3cfcd38f
Will Marceau <willfmarceau@gmail.com>
2025-04-08 17:53:22 -0700

    cleaned up client.c code

--- client.c
+++ client.c
@@ -1,8 +1,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-// might not need this one
-//#include <sys/un.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <stdio.h>
@@ -42,7 +40,6 @@ void addChannel(channelList *list, char *channelName) {
 	}
 
 	// add new channel to array
-	//list->channels[list->size] = (char *)malloc((strlen(channelName) + 1) * sizeof(char));
 	list->channels[list->size] = (char *)malloc(CHANNEL_MAX * sizeof(char));
 
 	// error checking
@@ -106,14 +103,6 @@ int main(int argc, char *argv[]) {
 		// extract command line arguments
 		const char *serverHost = argv[1];
 		char *serverPortString = argv[2];
-		//printf("%s\n", serverHost);
-
-		// if local host
-		// use gethostbyname()
-		//
-		//if (strcmp(serverHost, "localhost") == 0) {
-		//	serverHost = "127.0.0.1";
-		//}
 		
 		// resolve hostname
 		struct addrinfo hints, *resolution;
@@ -128,66 +117,16 @@ int main(int argc, char *argv[]) {
 		}
 		
 		
-		// convert to int for socket addr struct
-		//int serverPort = atoi(serverPortString);
-		//printf("%d\n", serverPort);
-		
-		// check validity of arguments
 
 		// create socket
-		//
-		// Not sure the first arg is correct
-		// SOCK_DGRAM for UDP?
 		int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
 		if (socketfd == -1) {
 			perror("socket Failure");
 		}
-	//	printf("%d\n", socketfd);
-		//
+		
 		// create surver add struct for connection
 		struct sockaddr_in clientAddr;
 		socklen_t clientLength = sizeof(clientAddr);
-
-		// clean struct
-		//memset(&serverAddr, 0, sizeof(serverAddr));
-		//serverAddr.sin_family = AF_INET;
-		//serverAddr.sin_port = htons(serverPort);
-		//serverAddr.sin_addr.s_addr = 
-		//struct sockaddr_in *serverAddr = (struct sockaddr_in *)resolution->ai_addr;
-
-		// convert string ip to usable format
-		//int convert = inet_pton(AF_INET, serverHost, &serverAddr.sin_addr);
-		
-		// error checking
-	//	if (convert == -1) { 
-		//	perror("errer converting address");
-		//}
-		//else if (convert == 0) {
-		//	perror("Invalid Address");
-		//}
-
-		//still need htonl, so store convert into a variable, then store it like slides?
-
-		// dont need this?
-		//serverAddr.sin_addr.s_addr = htonl(serverAddr.sin_addr);
-		// 
-		// connect to specified server
-		// dont connect? use sendto
-		//int err = connect(socketfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-		//if (err < 0) {
-			//perror("Error connecting to server");
-		//}
-
-		//printf("%s\n", argv[0]);
-
-		// check username length
-	//	if (strlen(arg[3]) > USERNAME_MAX) {
-		//       perror("username too long");
-		//}	       
-
-		//else {
-		//
-		//char[SAY_MAX] buff;
 
 		// create login request
 		struct request_login login_request;
@@ -196,11 +135,9 @@ int main(int argc, char *argv[]) {
 		// copy USERNAME_MAX chars from username pointer to request username
 		strncpy(login_request.req_username, argv[3], USERNAME_MAX - 1);
 		login_request.req_username[USERNAME_MAX - 1] = '\0';
-		//printf("%s\n", login_request.req_username);
 		fflush(stdout);
 
 		// send login request
-		//ssize_t bytes = sendto(socketfd, &login_request, sizeof(login_request), 0, (struct sockaddr*)&serverAddr, sizeof(*serverAddr));
 		ssize_t bytes = sendto(socketfd, &login_request, sizeof(login_request), 0, resolution->ai_addr, resolution->ai_addrlen);
 		
 		if (bytes < 0) {
@@ -215,7 +152,6 @@ int main(int argc, char *argv[]) {
 		join_common.req_channel[CHANNEL_MAX - 1] = '\0';
 
 		// send common request
-		//bytes = sendto(socketfd, &join_common, sizeof(join_common), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 		bytes = sendto(socketfd, &join_common, sizeof(join_common), 0, resolution->ai_addr, resolution->ai_addrlen);
 		if (bytes < 0) {
 			perror("failed to send log in request");
@@ -261,10 +197,8 @@ int main(int argc, char *argv[]) {
 
 		// start request loop
 		while (true) {
-			//printf(">");
 			count = 0;
-			// cant use fgets
-			//
+			
 			// initialize fd set
 			FD_ZERO(&readfds);
 
@@ -289,11 +223,9 @@ int main(int argc, char *argv[]) {
 				else {
 					// deal with when a client had started typing before message came in?
 					if (index > 0) {
-						//printf("yes\n");
-						//printf("%d\n", index);
 						fflush(stdout);
 
-						// print backspaces?
+						// print backspaces
 						for (int i = 0; i < index; i++) {
 							printf("\b \b");
 						}
@@ -304,14 +236,6 @@ int main(int argc, char *argv[]) {
 					// remove prompt
 					printf("\b \b");
 					fflush(stdout);
-
-
-					// could just do all the responses here instead
-					// of each function below?
-					//
-					// get rid of the set up like Existing channels from below
-					// and do them when the client recieves the corresponding
-					// packet
 
 					// create base struct
 					struct text *baseResponse = (struct text *)inBuff;
@@ -324,7 +248,6 @@ int main(int argc, char *argv[]) {
 
 						// check packet
 						if (inbytes == 132) {
-						//printf("%ld\n", inbytes);
 
 						// cast to say stuct
 						struct text_say *say = (struct text_say*)baseResponse;
@@ -447,29 +370,12 @@ int main(int argc, char *argv[]) {
 			}
 			// check for user input
 			if (FD_ISSET(0, &readfds)) {
-				//printf(">");
-				//fflush(stdout);
-
-				//index = 0;
-
-				// read character
-				// on enter break and deal with message
-				//
-				// use read instead
-				/*
-				while ((ch = getchar()) != '\n' && index < SAY_MAX - 1) {
-					// store typed char
-					buff[index++] = ch;
-				}
-				*/
 	
-
 				// read a char
 				int del = 0;
 				if (read(STDIN_FILENO, &ch, 1) > 0) {
 					if (ch == '\n') {
 						printf("\n");
-						//break;
 					}
 					// deal with backspace
 					else if (ch == 127 || ch == 8) {
@@ -493,15 +399,8 @@ int main(int argc, char *argv[]) {
 				if (ch == '\n') {
 				// clean-up
 				buff[index] = '\0';
-				//ch = getchar();
 				ch = '\0';
-				//printf("%s\n", buff);
 				index = 0;
-
-
-			
-
-		//	fgets(buff, sizeof(buff), stdin);
 
 			// copy message
 			strncpy(message, buff, SAY_MAX - 1);
@@ -524,11 +423,7 @@ int main(int argc, char *argv[]) {
 				token = strtok(NULL, " ");
 			}
 
-			//printf("buff: %s\n message: %s\n", buff, message);
-
 			buff[0] = '\0';
-			//printf("tokens: %s\n", tokens[0]);
-			//
 
 			if (count == 0) {
 				char empty[] = "";
@@ -655,15 +550,6 @@ int main(int argc, char *argv[]) {
 						if (bytes < 0) {
 							perror("Failed to send\n");
 						}
-
-				// set up
-				
-						// DONT THINK I NEED THIS
-				// get list response from servera
-				//
-				// when getting reasponse, loop until the header is the correct format
-				// to clear the cache of messages, parse any messages or other 
-				// packets if needed before completing the command
 					}
 
 
@@ -689,8 +575,6 @@ int main(int argc, char *argv[]) {
 						if (bytes < 0) {
 							perror("Failed to send\n");
 						}
-						
-						// set up
 
 					}
 
@@ -718,14 +602,6 @@ int main(int argc, char *argv[]) {
 								strncpy(activeChannel, tokens[1], CHANNEL_MAX - 1);
 								activeChannel[CHANNEL_MAX - 1] = '\0';
 								search = 1;
-								//populate join struct
-								//struct request_join join_request;
-								//join_request.req_type = REQ_JOIN;
-								//strncpy(join_request.req_channel, tokens[1], CHANNEL_MAX - 1);
-								//join_request.req_channel, tokens
-
-								// send join request
-								//bytes = sendto(socketfd, &join_request, sizeof(join_request), 0, resolution->ai_addr, resolution->ai_addrlen);
 							}
 						}
 
@@ -742,7 +618,6 @@ int main(int argc, char *argv[]) {
 			
 				// Unknown command
 				else {
-					// FIX THIS LATER
 					printf("*Unknown Command\n");
 					fflush(stdout);
 				}
@@ -750,7 +625,7 @@ int main(int argc, char *argv[]) {
 
 			else {
 				// send message
-				//
+				
 				// check if the user has an active channel
 				if (strcmp(activeChannel, "\0") != 0) {
 					// populate the say struct
